4c98a3e7e71deb8b47e0de5a833382d5b361e00e
Onur Altuğ <onur7altug@gmail.com>
2024-05-10 01:32:29 +0300

    delete unused import

--- lib/auth/auth_service.dart
+++ lib/auth/auth_service.dart
@@ -1,8 +1,5 @@
-import 'dart:math';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class AuthService {
   final _auth = FirebaseAuth.instance;
