3327a7175350d1902cd5a6c294920582a6c549e1
Onur Altuğ <onur7altug@gmail.com>
2024-05-23 16:37:53 +0300

    update main page view

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -81,67 +81,96 @@ class _MainPageState extends State<MainPage> {
                         ],
                       ),
                       const SizedBox(height: 50),
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => const HitchhikingFeatures()),
-                          );
-                        },
-                        icon: const Icon(Icons.assistant_navigation, size: 30, color: Colors.white),
-                        label: const Text('Hitchhiking', style: TextStyle(color: Colors.white)),
-                        style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(vertical: 20.0),
-                          textStyle: const TextStyle(fontSize: 20),
-                          backgroundColor: const Color(0xFFB71C1C),
+                      Container(
+                        margin: const EdgeInsets.only(left: 40.0, right: 40.0),
+                        child: ElevatedButton.icon(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => const HitchhikingFeatures()),
+                            );
+                          },
+                          icon: const Icon(Icons.assistant_navigation, size: 30, color: Colors.white),
+                          label: const Text('Hitchhiking', style: TextStyle(color: Colors.white)),
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(vertical: 20.0),
+                            textStyle: const TextStyle(fontSize: 20),
+                            backgroundColor: const Color(0xFFB71C1C),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                            ),
+                            side: const BorderSide(color: Colors.white, width: 1.0),
+                          ),
                         ),
                       ),
-                      const SizedBox(height: 25),
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => const SocialisationFeatures()),
-                          );
-                        },
-                        icon: const Icon(Icons.groups, size: 30, color: Colors.white),
-                        label: const Text('Socialisation', style: TextStyle(color: Colors.white)),
-                        style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(vertical: 20.0),
-                          textStyle: const TextStyle(fontSize: 20),
-                          backgroundColor: const Color(0xFFB71C1C),
+                      const SizedBox(height: 35),
+                      Container(
+                        margin: const EdgeInsets.only(left: 40.0, right: 40.0),
+                        child: ElevatedButton.icon(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => const SocialisationFeatures()),
+                            );
+                          },
+                          icon: const Icon(Icons.groups, size: 30, color: Colors.white),
+                          label: const Text('Socialisation', style: TextStyle(color: Colors.white)),
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(vertical: 20.0),
+                            textStyle: const TextStyle(fontSize: 20),
+                            backgroundColor: const Color(0xFFB71C1C),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                            ),
+                            side: const BorderSide(color: Colors.white, width: 1.0),
+                          ),
                         ),
                       ),
-                      const SizedBox(height: 25),
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => const LostPropertyFeatures()),
-                          );
-                        },
-                        icon: const Icon(Icons.search, size: 30, color: Colors.white),
-                        label: const Text('Lost Property', style: TextStyle(color: Colors.white)),
-                        style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(vertical: 20.0),
-                          textStyle: const TextStyle(fontSize: 20),
-                          backgroundColor: const Color(0xFFB71C1C),
+                      const SizedBox(height: 35),
+                      Container(
+                        margin: const EdgeInsets.only(left: 40.0, right: 40.0),
+                        child: ElevatedButton.icon(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => const LostPropertyFeatures()),
+                            );
+                          },
+                          icon: const Icon(Icons.search, size: 30, color: Colors.white),
+                          label: const Text('Lost Property', style: TextStyle(color: Colors.white)),
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(vertical: 20.0),
+                            textStyle: const TextStyle(fontSize: 20),
+                            backgroundColor: const Color(0xFFB71C1C),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                            ),
+                            side: const BorderSide(color: Colors.white, width: 1.0),
+                          ),
                         ),
                       ),
-                      const SizedBox(height: 25),
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => const AccommodationFeatures()),
-                          );
-                        },
-                        icon: const Icon(Icons.home, size: 30, color: Colors.white),
-                        label: const Text('Accommodation', style: TextStyle(color: Colors.white)),
-                        style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(vertical: 20.0),
-                          textStyle: const TextStyle(fontSize: 20),
-                          backgroundColor: const Color(0xFFB71C1C),
+
+                      const SizedBox(height: 35),
+                      Container(
+                        margin: const EdgeInsets.only(left: 40.0, right: 40.0),
+                        child: ElevatedButton.icon(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => const AccommodationFeatures()),
+                            );
+                          },
+                          icon: const Icon(Icons.home, size: 30, color: Colors.white),
+                          label: const Text('Accommodation', style: TextStyle(color: Colors.white)),
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(vertical: 20.0),
+                            textStyle: const TextStyle(fontSize: 20),
+                            backgroundColor: const Color(0xFFB71C1C),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                            ),
+                            side: const BorderSide(color: Colors.white, width: 1.0),
+                          ),
                         ),
                       ),
                     ],
